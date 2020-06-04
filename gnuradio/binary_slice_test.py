9b961aa8599e57107d65ce511ae81babe35be849
Laykel <layke.luc@gmail.com>
2020-06-04 23:13:15 +0200

    More recordings, more attempts at decoding

--- gnuradio/binary_slice_test.py
+++ gnuradio/binary_slice_test.py
@@ -27,13 +27,14 @@ from gnuradio.filter import firdes
 import sip
 from gnuradio import blocks
 import pmt
-from gnuradio import filter
+from gnuradio import digital
 from gnuradio import gr
 import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio.qtgui import Range, RangeWidget
 from gnuradio import qtgui
 
 class binary_slice_test(gr.top_block, Qt.QWidget):
@@ -73,12 +74,20 @@ class binary_slice_test(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 2e6
+        self.miller_offset = miller_offset = -160e-3
+        self.manchester_offset = manchester_offset = 0
 
         ##################################################
         # Blocks
         ##################################################
+        self._miller_offset_range = Range(-300e-3, 100e-3, 100e-4, -160e-3, 200)
+        self._miller_offset_win = RangeWidget(self._miller_offset_range, self.set_miller_offset, 'miller_offset', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._miller_offset_win)
+        self._manchester_offset_range = Range(-300e-3, 100e-3, 100e-4, 0, 200)
+        self._manchester_offset_win = RangeWidget(self._manchester_offset_range, self.set_manchester_offset, 'manchester_offset', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._manchester_offset_win)
         self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            8192, #size
+            120000, #size
             samp_rate, #samp_rate
             "", #name
             2 #number of inputs
@@ -124,34 +133,29 @@ class binary_slice_test(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.low_pass_filter_0 = filter.fir_filter_ccf(
-            1,
-            firdes.low_pass(
-                1,
-                samp_rate,
-                40e3,
-                4003,
-                firdes.WIN_HAMMING,
-                6.76))
+        self.digital_binary_slicer_fb_0_0 = digital.binary_slicer_fb()
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/luc/HEIG/TB/REPO_nfc-rf-fingerprinting/data/raw/airspyhf+/test-tag3.nfc', True, 0, 0)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/luc/HEIG/TB/REPO_nfc-rf-fingerprinting/data/raw/airspyhf+/tag1-2M-13.562M-0-16-16.nfc', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/luc/HEIG/TB/REPO_nfc-rf-fingerprinting/data/cut-signals/tag3-grc-test.bin', False)
+        self.blocks_file_sink_0.set_unbuffered(True)
         self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
-        self.blocks_add_const_vxx_0_0 = blocks.add_const_ff(-0.2)
-        self.blocks_add_const_vxx_0 = blocks.add_const_ff(-0.1)
+        self.blocks_add_const_vxx_0_0 = blocks.add_const_ff(manchester_offset)
+        self.blocks_add_const_vxx_0 = blocks.add_const_ff(miller_offset)
 
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.digital_binary_slicer_fb_0_0, 0))
         self.connect((self.blocks_add_const_vxx_0, 0), (self.qtgui_time_sink_x_0, 1))
         self.connect((self.blocks_add_const_vxx_0_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_add_const_vxx_0, 0))
         self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_add_const_vxx_0_0, 0))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.low_pass_filter_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.digital_binary_slicer_fb_0_0, 0), (self.blocks_file_sink_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "binary_slice_test")
@@ -164,8 +168,21 @@ class binary_slice_test(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 40e3, 4003, firdes.WIN_HAMMING, 6.76))
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+
+    def get_miller_offset(self):
+        return self.miller_offset
+
+    def set_miller_offset(self, miller_offset):
+        self.miller_offset = miller_offset
+        self.blocks_add_const_vxx_0.set_k(self.miller_offset)
+
+    def get_manchester_offset(self):
+        return self.manchester_offset
+
+    def set_manchester_offset(self, manchester_offset):
+        self.manchester_offset = manchester_offset
+        self.blocks_add_const_vxx_0_0.set_k(self.manchester_offset)
 
 
 
