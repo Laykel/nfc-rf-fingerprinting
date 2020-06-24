b0420cbedad158f427a35a03d24af55706676d86
Laykel <layke.luc@gmail.com>
2020-06-24 20:08:56 +0200

    Updated GRC3.7 flowgraphs

--- gnuradio/GRC3.7/nfc_simplest_capture_37.py
+++ gnuradio/GRC3.7/nfc_simplest_capture_37.py
@@ -7,17 +7,6 @@
 # GNU Radio version: 3.7.13.5
 ##################################################
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-
-from PyQt4 import Qt
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
@@ -25,37 +14,13 @@ from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import osmosdr
-import sys
 import time
-from gnuradio import qtgui
 
 
-class nfc_simplest_capture_37(gr.top_block, Qt.QWidget):
+class nfc_simplest_capture_37(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "NFC Simplest Capture GRC3.7")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("NFC Simplest Capture GRC3.7")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "nfc_simplest_capture_37")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
-
 
         ##################################################
         # Variables
@@ -79,7 +44,7 @@ class nfc_simplest_capture_37(gr.top_block, Qt.QWidget):
         self.osmosdr_source_0.set_antenna('', 0)
         self.osmosdr_source_0.set_bandwidth(0, 0)
 
-        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'C:\\Users\\Luc\\Desktop\\test.bin', False)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'C:\\Users\\Luc\\Desktop\\dev\\TB\\TestSignals\\768k-tests\\tag1-Nexus6p.nfc', False)
         self.blocks_file_sink_0_0.set_unbuffered(False)
 
 
@@ -88,11 +53,6 @@ class nfc_simplest_capture_37(gr.top_block, Qt.QWidget):
         # Connections
         ##################################################
         self.connect((self.osmosdr_source_0, 0), (self.blocks_file_sink_0_0, 0))
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "nfc_simplest_capture_37")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -111,21 +71,14 @@ class nfc_simplest_capture_37(gr.top_block, Qt.QWidget):
 
 def main(top_block_cls=nfc_simplest_capture_37, options=None):
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
     tb.start()
-    tb.show()
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
+    try:
+        raw_input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
 
 
 if __name__ == '__main__':
