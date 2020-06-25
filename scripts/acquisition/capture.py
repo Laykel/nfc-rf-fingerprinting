274b5d5769c601ecbe854d9a593972154baa1792
Laykel <layke.luc@gmail.com>
2020-06-25 15:21:43 +0200

    Add center frequency as a parameter

--- scripts/acquisition/capture.py
+++ scripts/acquisition/capture.py
@@ -10,7 +10,7 @@ from gnuradio import gr
 
 """
 Uses the osmosdr sink block to read the output of an AirspyHF+ device, reads a specified amount of samples
-(time in seconds * sample rate) and writes the output in a specified file.
+(time in seconds * sample rate) at a given frequency and writes the output in a specified file.
 
 Based on a GNU Radio Python Flow Graph
 Requires GNU Radio version: 3.7.x or 3.8.x
@@ -19,18 +19,15 @@ Author: Luc Wachter
 
 
 class SimplestCapture(gr.top_block):
-    def __init__(self, capture_length, sample_rate, output):
+    def __init__(self, capture_length, sample_rate, center_frequency, output):
         gr.top_block.__init__(self, "Simplest Capture")
-
-        # Variables
-        seconds = capture_length
 
         # Blocks
         source = osmosdr.source(
             args="numchan=" + str(1) + " " + 'airspyhf=0'
         )
         source.set_sample_rate(sample_rate)
-        source.set_center_freq(13.56e6, 0)
+        source.set_center_freq(center_frequency, 0)
         source.set_freq_corr(0, 0)
         source.set_gain(16, 0)
         source.set_if_gain(16, 0)
@@ -38,9 +35,9 @@ class SimplestCapture(gr.top_block):
         source.set_antenna('', 0)
         source.set_bandwidth(0, 0)
 
-        head = blocks.head(gr.sizeof_gr_complex*1, int(seconds * sample_rate))
+        head = blocks.head(gr.sizeof_gr_complex * 1, int(capture_length * sample_rate))
 
-        sink = blocks.file_sink(gr.sizeof_gr_complex*1, output, False)
+        sink = blocks.file_sink(gr.sizeof_gr_complex * 1, output, False)
         sink.set_unbuffered(False)
 
         # Connections
@@ -49,20 +46,27 @@ class SimplestCapture(gr.top_block):
 
 
 def main():
+    # Manage program arguments
     parser = ArgumentParser(description="GNURadio-based capture script using airspyhf+")
+
+    parser.add_argument("path", help="The path to the output file relative to the script's location")
     parser.add_argument("--time", help="The capture length in seconds", default=5, type=int)
     parser.add_argument("--samplerate", help="The (theoretical) number of to capture samples per second",
                         default=768000, type=int)
-    parser.add_argument("path", help="The path to the output file relative to the script's location")
+    parser.add_argument("--freq", help="The center frequency (in Hertz)", default=int(13.56e6), type=int)
 
     args = parser.parse_args()
-    SECONDS = args.time
-    SAMPLE_RATE = args.samplerate
-    PATH = os.path.join(os.getcwd(), args.path)
+    seconds = args.time
+    sample_rate = args.samplerate
+    center_freq = args.freq
+    path = os.path.join(os.getcwd(), args.path)
 
-    tb = SimplestCapture(SECONDS, SAMPLE_RATE, PATH)
+    # Execute flow graph with given parameters
+    tb = SimplestCapture(seconds, sample_rate, center_freq, path)
+
+    # TODO Find out why neither the standard "Run to completion" nor the following code work on GR 4.7
     tb.start()
-    time.sleep(SECONDS + 2)
+    time.sleep(seconds + 2)
     tb.stop()
     tb.wait()
 
